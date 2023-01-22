4ffedc6f72118223b1613ec6ccdd9e7cfa94e4a6
yuri_sn <yuri.snitko@gmail.com>
2023-01-22 17:01:07 +0300

    html css js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ sliders.forEach(sliderEl => {
 // выпадающее меню
 function dropCity() {
   document.getElementById("city").style.display = "block";
-  document.getElementById("lang").style.display = "block";
   document.getElementById("arr_1").style.transform = 'rotate(180deg)';
   document.getElementById("arrow_1").style.fill = '#E55241';
   document.getElementById('town').style.color = '#E55241';
@@ -36,21 +35,58 @@ function dropCity() {
 }
 
 function dropLang() {
-document.getElementById("lang_menu").style.display = "block";
-document.getElementById("arr_2").style.transform = 'rotate(180deg)';
-document.getElementById("arrow_2").style.fill = '#E55241';
-document.getElementById('lang').style.color = '#E55241';
-document.getElementById("earth").style.fill = '#E55241';
+  document.getElementById("lang_menu").style.display = "block";
+  document.getElementById("arr_2").style.transform = 'rotate(180deg)';
+  document.getElementById("arrow_2").style.fill = '#E55241';
+  document.getElementById('lang').style.color = '#E55241';
+  document.getElementById("earth").style.fill = '#E55241';
 }
 
 function dropAccount() {
-document.getElementById("account_menu").style.display = "block";
-document.getElementById("arr_3").style.transform = 'rotate(180deg)';
-document.getElementById("arrow_3").style.fill = '#E55241';
-document.getElementById('acc').style.color = '#E55241';
-document.getElementById("user_1").style.fill = '#E55241';
-document.getElementById("user_2").style.fill = '#E55241';
-}
+  document.getElementById("account_menu").style.display = "block";
+  document.getElementById("arr_3").style.transform = 'rotate(180deg)';
+  document.getElementById("arrow_3").style.fill = '#E55241';
+  document.getElementById('acc').style.color = '#E55241';
+  document.getElementById("user_1").style.fill = '#E55241';
+  document.getElementById("user_2").style.fill = '#E55241';
+ }
+
+// закрытие меню по клику вне блока
+document.addEventListener("mouseup", function(event) {
+  const cityShow = document.getElementById("city");
+  if (!cityShow.contains(event.target)) {
+      cityShow.style.display = 'none'
+      document.getElementById('clouds').style.fill = '#3A3E48';
+      document.getElementById("arr_1").style.transform = 'rotate(0deg)';
+      document.getElementById("arrow_1").style.fill = '#3A3E48';
+      document.getElementById('town').style.color = '#3A3E48';
+      document.getElementById('temp').style.color = '#3A3E48';
+      document.getElementById('clouds').style.fill = '#3A3E48';
+  }
+});
+
+document.addEventListener("mouseup", function(event) {
+  const langShow = document.getElementById("lang_menu");
+  if (!langShow.contains(event.target)) {
+    langShow.style.display = 'none';
+    document.getElementById("arr_2").style.transform = 'rotate(0deg)';
+    document.getElementById("arrow_2").style.fill = '#3A3E48';
+    document.getElementById('lang').style.color = '#3A3E48';
+    document.getElementById("earth").style.fill = '#3A3E48';
+  }
+});
+
+document.addEventListener("mouseup", function(event) {
+  const accShow = document.getElementById("account_menu");
+  if (!accShow.contains(event.target)) {
+    accShow.style.display = 'none';
+    document.getElementById("arr_3").style.transform = 'rotate(0deg)';
+    document.getElementById("arrow_3").style.fill = '#3A3E48';
+    document.getElementById('acc').style.color = '#3A3E48';
+    document.getElementById("user_1").style.fill = '#3A3E48';
+    document.getElementById("user_2").style.fill = '#3A3E48';
+  }
+});
 
 // поле поиск
 function btnOn() {
@@ -80,8 +116,8 @@ const uid_len_email = emailValid.value.length;
 const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
   
       if (uid_len == 0)  {
-        checkEmpty.style.borderColor = 'red';
-        checkEmpty.style.borderWidth = '2px';
+        checkEmpty.style.borderColor = '#E55241';
+        checkEmpty.style.borderWidth = '1px';
         checkEmpty.placeholder = 'Введите имя';
       }
       
@@ -90,11 +126,11 @@ const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].
       }
   
       if (uid_len_last == 0) {
-        checkEmptyname.style.borderColor = 'red';
-        checkEmptyname.style.borderWidth = '2px';
+        checkEmptyname.style.borderColor = '#E55241';
+        checkEmptyname.style.borderWidth = '1px';
         checkEmptyname.placeholder = 'Введите пароль';
-        document.getElementById('numbers').style.borderColor = 'red';
-        document.getElementById('numbers').style.borderWidth = '2px';
+        document.getElementById('numbers').style.borderColor = '#E55241';
+        document.getElementById('numbers').style.borderWidth = '1px';
 
       }
       
@@ -104,8 +140,8 @@ const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].
       }
   
       if (uid_len_pass == 0) {
-        passEmpty.style.borderColor = 'red';
-        passEmpty.style.borderWidth = '2px'
+        passEmpty.style.borderColor = '#E55241';
+        passEmpty.style.borderWidth = '1px'
         passEmpty.placeholder = 'Введите правильный ответ';
       }
       
@@ -114,8 +150,8 @@ const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].
       }
 
       if (uid_len_email == 0) {
-        emailValid.style.borderColor = 'red';
-        emailValid.style.borderWidth = '2px';
+        emailValid.style.borderColor = '#E55241';
+        emailValid.style.borderWidth = '1px';
         emailValid.placeholder = 'Введите электронной почты';
       }
       
@@ -127,8 +163,8 @@ const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].
         emailValid.style.borderColor = 'limegreen';
   
       } else {
-        emailValid.style.borderColor = 'red';
-        emailValid.style.borderWidth = '2px';
+        emailValid.style.borderColor = '#E55241';
+        emailValid.style.borderWidth = '1px';
         alert('введите правильный адрес электронной почты');
       }
   
