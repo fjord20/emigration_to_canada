1aead7a15ccad077dece7b387d0391d5041cb787
yuri_sn <yuri.snitko@gmail.com>
2023-02-03 22:22:02 +0300

    html css js

--- js/script.js
+++ js/script.js
@@ -385,6 +385,35 @@ function userid_validation_() {
     document.getElementById('ham_close').style.display = 'none';
   }
 
+//добавление в закладки сайта
+function add_favorite(a) {
+  title=document.title;
+  url=document.location;
+  try {
+    // Internet Explorer
+    eval("window.external.AddFa-vorite(url, title)".replace(/-/g,''));
+  }
+  catch (e) {
+    try {
+      // Mozilla
+      window.sidebar.addPanel(title, url, "");
+    }
+    catch (e) {
+      // Opera
+      if (typeof(opera)=="object") {
+        a.rel="sidebar";
+        a.title=title;
+        a.url=url;
+        return true;
+      }
+      else {
+        // Остальные браузеры
+        alert('Нажмите Ctrl + D, чтобы добавить страницу в закладки');
+      }
+    }
+  }
+  return false;
+}
 //   let isListShow = false;
 //   let drop = document.querySelector('.btn_arrow');
 //   if (drop !== null) {
